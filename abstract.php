349784c43269a4491e2f434768d95fe7e87f43e5
suresh <surimulkanuri@gmail.com>
2021-05-18 10:55:55 +0530

    Add files via upload
    new changes

---
+++ abstract.php
@@ -0,0 +1,186 @@
+ <?php include("includes/header.php"); ?> 
+
+
+
+      <section class="page-title-area" style="background-image: url(assets/images/bg.jpg);">
+         <div class="container">
+            <h2 class="title">Abstract</h2>
+            <!--  <ul class="breadcrumb-nav">
+               <li><a href="index.php">Home</a></li>
+               <li class="active">About</li>
+               </ul> -->
+         </div>
+      </section>
+      <!--====== Why Choose Us Start ======-->
+      <section class="whu-section section-gap abt soft-blue-bg abstract-inner">
+         <div class="container">
+            <section class="conatct-section section-gap abstract">
+               <div class="contact-form-area wow fadeInUp" data-wow-delay="0.3s">
+                  <div class="col-lg-12">
+                     <div class="contact-form registration">
+                        <form action="#">
+                           
+                           <div class="row cpm-frm">
+                              <div class="col-lg-6">
+                                 <div class="input-group">
+                                    <select>
+                                       <option>Select Title</option>
+                                       <option>Dr.</option>
+                                       <option>Mr.</option>
+                                       <option>Mrs.</option>
+                                       <option>Ms.</option>
+                                    </select>
+                                 </div>
+                              </div>
+                              <div class="col-lg-6">
+                                 <div class="input-group">
+                                    <input type="text" placeholder="Full Name Here">
+                                    <span class="icon"><i class="fal fa-user"></i></span>
+                                 </div>
+                              </div>
+                              <div class="col-lg-6">
+                                 <div class="input-group">
+                                    <input type="email" placeholder="Email Here">
+                                    <span class="icon"><i class="fal fa-envelope-open"></i></span>
+                                 </div>
+                              </div>
+                              <div class="col-lg-6">
+                                 <div class="input-group">
+                                    <input type="text" placeholder="Phone No">
+                                    <span class="icon"><i class="fal fa-phone"></i></span>
+                                 </div>
+                              </div>
+                              <div class="col-lg-6">
+                                 <div class="input-group">
+                                    <select>
+                                       <option>Select Country</option>
+                                       <option value="United States">United States</option>
+                                       <option value="United Kingdom">United Kingdom</option>
+                                       <option value="Afghanistan">Afghanistan</option>
+
+<option value="United States">United States</option>
+                                       <option value="United Kingdom">United Kingdom</option>
+                                       <option value="Afghanistan">Afghanistan</option>
+
+                                      <option value="United States">United States</option>
+                                       <option value="United Kingdom">United Kingdom</option>
+                                       <option value="Afghanistan">Afghanistan</option>
+                                       <option value="United States">United States</option>
+                                       <option value="United Kingdom">United Kingdom</option>
+                                       <option value="Afghanistan">Afghanistan</option>                                       
+                                       <option value="United States">United States</option>
+                                       <option value="United Kingdom">United Kingdom</option>
+                                       <option value="Afghanistan">Afghanistan</option>
+                                    </select>
+                                 </div>
+                              </div>
+                              <div class="col-lg-6">
+                                 <div class="input-group">
+                                    <select>
+                                        <option value="">Please Select session</option>
+                                                                        <option value="Quantum Devices">Quantum Devices</option>
+                                                                        <option value="Quantum Spectroscopy">Quantum Spectroscopy</option>
+                                                                        <option value="New Quantum Structures ">New Quantum Structures </option>
+                                                                        <option value="Nonlinear and Ultrafast Optical Phenomena">Nonlinear and Ultrafast Optical Phenomena</option>
+                                                                        <option value="Hyperbolic Metamaterials">Hyperbolic Metamaterials</option>
+                                                                        <option value="Carbonaceous Materials">Carbonaceous Materials</option>
+                                                                        <option value="Characterization (Optical, Electrical, Structural, etc.)">Characterization (Optical, Electrical, Structural, etc.)</option>
+                                                                        <option value="Micro- and Nano-Fabrication Technologies">Micro- and Nano-Fabrication Technologies</option>
+                                                                        <option value="Semiconductor Quantum Detectors">Semiconductor Quantum Detectors</option>
+                                                                        <option value="Wide-bandgap Solar-blind Photodetectors &amp; Focal Plane arrays">Wide-bandgap Solar-blind Photodetectors &amp; Focal Plane arrays</option>
+                                                                        <option value="Nano-Photonics, Metamaterials, Active Plasmonics">Nano-Photonics, Metamaterials, Active Plasmonics</option>
+                                                                        <option value="THz Emitters and Receivers">THz Emitters and Receivers</option>
+                                                                        <option value="Single-photon Counting Detectors and Biosensing">Single-photon Counting Detectors and Biosensing</option>
+                                                                        <option value="Terahertz Nano-Photonics and Nanoelectronics">Terahertz Nano-Photonics and Nanoelectronics</option>
+                                                                        <option value="Near-Field Optics and Flat Optics">Near-Field Optics and Flat Optics</option>
+                                                                        <option value="Bio-Electronics and Bio-Photonics">Bio-Electronics and Bio-Photonics</option>
+                                                                        <option value="Light-Matter Interaction at the Nano-Scale">Light-Matter Interaction at the Nano-Scale</option>
+                                                                        <option value="Nano-Structured and Functionalized Surfaces">Nano-Structured and Functionalized Surfaces</option>
+                                                                        <option value="Photonic Bandgap Structures and their Applications">Photonic Bandgap Structures and their Applications</option>
+                                                                        <option value="Neuromorphic Devices, Circuits, and Systems">Neuromorphic Devices, Circuits, and Systems</option>
+                                                                        <option value="Metrology ">Metrology </option>
+                                                                        <option value="Magnonics and  its Applications">Magnonics and  its Applications</option>
+                                                                        <option value="Spintronic GHz to THz Sources and Detectors">Spintronic GHz to THz Sources and Detectors</option>
+                                                                        <option value="Topological Devices and Materials">Topological Devices and Materials</option>
+                                                                        <option value="Quantum Optical Measurements and Decoherence Effects ">Quantum Optical Measurements and Decoherence Effects </option>
+                                                                        <option value="Quantum Memories ">Quantum Memories </option>
+                                                                        <option value="Integrated Photonics for Quantum Sensing">Integrated Photonics for Quantum Sensing</option>
+                                                                        <option value="Phononics">Phononics</option>
+                                                                        <option value="Spinorbitronics">Spinorbitronics</option>
+                                                                        <option value="Superconducting Oxides">Superconducting Oxides</option>
+                                                                       <option value="Others">
+                                    Others
+                                   </option>
+                                    </select>
+                                 </div>
+                              </div>
+                              <div class="col-lg-6">
+                                 <div class="input-group">
+                                    <input type="text" placeholder="Organaization Name">
+                                    <span class="icon"><i class="fal fa-sitemap"></i></span>
+                                 </div>
+                              </div>
+                               <div class="col-lg-6">
+                                 <div class="input-group">
+                                    <select>
+                                       <option value="" selected="">
+                              Interested in
+                           </option>
+                      <!--<option value="Speaking">
+                        Speaking
+                      </option>-->
+                    <option value="Poster Presentation">
+                     Poster Presentation
+                    </option>
+                    <option value="Oral Presentation">
+                     Oral Presentation
+                    </option>
+                    <option value="Conducting Workshop">
+                     Conducting Workshop
+                    </option>
+                    <option value="Conducting Symposium">
+                     Conducting Symposium
+                    </option>
+                    <option value="Young Researcher's Forum(YRF)">
+                     Young Researcher's Forum(YRF)
+                    </option>
+                    <option value="Others">
+                     Others
+                    </option>
+                                    </select>
+                                 </div>
+                              </div>
+
+                              <div class="col-md-12 choose-file">
+                 <input class="form-control" type="file" name="uploadfile" placeholder="Upload Abstract" required="">
+               </div>
+
+                             <div class="col-md-12 download-file">
+                                <a href="https://phronesisonline.com/abstract-template.docx">
+                                 <button type="button" class="btn"><i class="fa fa-download"></i> Download</button></a>
+                             </div>
+                             <div class="col-lg-12 text-center" style="padding-bottom:22px">
+                  <div class="g-recaptcha" data-sitekey="6LcMmL4UAAAAAAOIeoiXzVmJ_Dhx8pThzwvwCSua"></div>
+                </div>
+                              <div class="col-12">
+                                 <div class="input-group textarea-group mt-30">
+                                    <button class="main-btn small-size rounded-btn icon-right">Submit Abstract<i class="fal fa-long-arrow-right"></i></button>
+                                 </div>
+                              </div>
+                              <!-- <div class="form-group">
+                                 <input class="btn" name="submit" type="submit" value="Submit Abstract">
+                                 </div> -->
+                           </div>
+                        </form>
+                     </div>
+                  </div>
+               </div>
+            </section>
+           
+            
+          
+         </div>
+         </div>
+      </section>
+      <!--====== Why Choose Us End ======-->
+      <?php include("includes/footer.php"); ?> 
